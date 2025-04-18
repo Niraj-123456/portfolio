ab3e52f5f2987bb4860adb1672386bf4a32eb894
Niraj Lama <nlama@hamropatro.com>
2025-04-18 20:36:45 +0545

    integrated analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
 	import Header from '$lib/header/Header.svelte';
 	import Footer from '$lib/footer/Footer.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main class="w-full min-h-screen">
